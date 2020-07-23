d33cf51488d061c7b4ab45e1af77d50a81187f64
Spandan Das <spandand515@gmail.com>
2020-07-23 16:22:56 -0400

    Update train.py

--- NCCS/train.py
+++ NCCS/train.py
@@ -11,14 +11,17 @@ from collections import Counter
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import confusion_matrix, roc_auc_score
 from sklearn.naive_bayes import GaussianNB
+from sklearn.svm import LinearSVC
+from sklearn.linear_model import LogisticRegression
 from xgboost import XGBClassifier
+
 from data_loader import get_data
 
 
 def random_forest(X, y, vb=False):
     # Random Forest Hyperparameters
     n_estimators = 500
-    max_depth = 15
+    max_depth = 20
     bootstrap = True
     criterion = 'entropy'
     class_weight = 'balanced_subsample'
@@ -30,7 +33,7 @@ def random_forest(X, y, vb=False):
                                  max_depth=max_depth, oob_score=False, verbose=verbose,
                                  class_weight=class_weight, random_state=random_state, n_jobs=n_job)
 
-    rfc.fit(X, y)
+    rfc.fit(X, np.argmax(y, axis=1))
     return rfc
 
 
@@ -46,40 +49,63 @@ def xgboost_clf(X, y, vb=False):
     return xgb
 
 
+def svm(X, y, vb=False):
+    svc = LinearSVC()
+    svc.fit(X, np.argmax(y, axis=1))
+    return svc
+
+
+def logistic_regression(X, y, vb=False):
+    class_weight = 'balanced'
+    n_jobs = -1
+    lr = LogisticRegression(class_weight=class_weight, n_jobs=n_jobs)
+    lr.fit(X, np.argmax(y, axis=1))
+    return lr
+
+
 def build_train_model(model_name, X, y, vb=False):
     f_name = {'random_forest': random_forest,
               'naive_bayes': naive_bayes,
-              'xgboost_clf': xgboost_clf}
+              'xgboost_clf': xgboost_clf,
+              'svm': svm,
+              'logistic_regression': logistic_regression}
 
     return f_name[model_name](X, y, vb=vb)
 
 
-def evaluate(model, X, y):
+def evaluate(model, X, y, model_name='random_forest'):
     y_pred = model.predict(X)
-    # y_pred = np.argmax(y_pred, axis=1)
+    # if isEnsemble: y_pred = np.argmax(y_pred, axis=1)
     conf_mat = confusion_matrix(np.argmax(y, axis=1), y_pred)
     print(f'\n\n{conf_mat}\n\n')
 
     print("Accuracy: ", np.trace(conf_mat) / float(np.sum(conf_mat)), end='\n\n')
+
+    if model_name in {'svm'}:
+        return
+
     y_probs = model.predict_proba(X)
-    # print(y_probs.shape, '\n', y_probs)
-    # y_pred_probs = np.column_stack(tuple(y_probs[i][:, 1] for i in range(y.shape[1])))
-    auc_roc = roc_auc_score(y, y_probs)
+    if model_name == 'random_forest':
+        y_pred_probs = np.column_stack(tuple(y_probs[i][:, 1] for i in range(y.shape[1])))
+        auc_roc = roc_auc_score(y, y_pred_probs)
+    else:
+        auc_roc = roc_auc_score(y, y_probs)
+        print(y_probs.shape, '\n', y_probs)
     print("AUC ROC:", auc_roc)
 
 
-def train(model_name='random_forest', verbose=False):
-    X_train, X_test, y_train, y_test = get_data()
+def train_model(model_name='random_forest', verbose=False):
+    X_train, X_test, y_train, y_test = get_data(num_days_per_month=7)
 
     # train model
     model = build_train_model(model_name, X_train, y_train, vb=verbose)
 
     # evaluate model
-    evaluate(model, X_test, y_test)
+    evaluate(model, X_test, y_test, model_name=model_name)
 
     # save model
     os.chdir("../../../../../../home/sdas11/")
     joblib.dump(model, f'{model_name}.model')
 
 
-train(model_name='xgboost_clf', verbose=True)
+train_model(model_name='logistic_regression', verbose=True)
